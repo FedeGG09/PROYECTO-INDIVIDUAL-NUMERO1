dfff0ae4456eee05959f614ac55c375492770016
FedeGG09 <124220922+FedeGG09@users.noreply.github.com>
2023-08-04 19:07:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,23 @@ import pandas as pd
 app = FastAPI()
 
 # Cargamos el dataset en un DataFrame
-films= pd.read_csv('Films.csv')
-
+films = pd.read_csv('Films.csv')
 
 # Funciones para los endpoints solicitados
 
 @app.get('/peliculas_idioma/')
 def peliculas_idioma(Idioma: str):
-    count_peliculas = moviesdataset[moviesdataset['original_language'] == Idioma].shape[0]
+    count_peliculas = films[films['original_language'] == Idioma].shape[0]
     return f"{count_peliculas} películas fueron estrenadas en idioma {Idioma}"
 
 @app.get('/peliculas_duracion/')
 def peliculas_duracion(Pelicula: str):
-    movie_data = moviesdataset[moviesdataset['original_title'] == Pelicula].iloc[0]
+    movie_data = films[films['original_title'] == Pelicula].iloc[0]
     return f"{Pelicula}. Duración: {movie_data['runtime']}. Año: {movie_data['release_date'][-4:]}"
 
 @app.get('/franquicia/')
 def franquicia(Franquicia: str):
-    franquicia_data = moviesdataset[moviesdataset['belongs_to_collection'] == Franquicia]
+    franquicia_data = films[films['belongs_to_collection'] == Franquicia]
     peliculas_count = franquicia_data.shape[0]
     ganancia_total = franquicia_data['revenue'].sum()
     ganancia_promedio = ganancia_total / peliculas_count
@@ -29,12 +28,12 @@ def franquicia(Franquicia: str):
 
 @app.get('/peliculas_pais/')
 def peliculas_pais(Pais: str):
-    count_peliculas = moviesdataset[moviesdataset['production_countries'] == Pais].shape[0]
+    count_peliculas = films[films['production_countries'] == Pais].shape[0]
     return f"Se produjeron {count_peliculas} películas en el país {Pais}"
 
 @app.get('/productoras_exitosas/')
 def productoras_exitosas(Productora: str):
-    productora_data = moviesdataset[moviesdataset['production_companies'] == Productora]
+    productora_data = films[films['production_companies'] == Productora]
     revenue_total = productora_data['revenue'].sum()
     peliculas_count = productora_data.shape[0]
     return f"La productora {Productora} ha tenido un revenue de {revenue_total} y ha realizado {peliculas_count} películas"
@@ -42,7 +41,7 @@ def productoras_exitosas(Productora: str):
 @app.get('/get_director/')
 def get_director(nombre_director: str):
     resultado = []
-    director_data = moviesdataset[moviesdataset['director'] == nombre_director]
+    director_data = films[films['director'] == nombre_director]
     
     if not director_data.empty:
         for _, row in director_data.iterrows():
@@ -60,7 +59,7 @@ def get_director(nombre_director: str):
 
 @app.get('/peliculas_idioma_info/')
 def peliculas_idioma_info(Idioma: str):
-    idioma_data = moviesdataset[moviesdataset['original_language'] == Idioma]
+    idioma_data = films[films['original_language'] == Idioma]
     if not idioma_data.empty:
         peliculas_info = []
         for _, row in idioma_data.iterrows():
